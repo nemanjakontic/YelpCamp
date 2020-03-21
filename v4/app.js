a078050bfa72c0b057253c5428b75de1bd7660e2
nemanjakontic <nemanja.kontic28@gmail.com>
2020-03-21 10:47:10 +0100

    omoguceno dodavanje komentara

--- v4/app.js
+++ v4/app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var Campground = require("./models/campgrounds");
+var Comment = require("./models/comment");
 var seedDB = require("./seeds")
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,14 +30,14 @@ app.get("/campgrounds", function(req, res) {
         if (err) {
             console.log("greska");
         } else {
-            res.render("index", { campgrounds: allCampgrounds });
+            res.render("campgrounds/index", { campgrounds: allCampgrounds });
         }
     });
 });
 
 //CREATE
 app.get("/campgrounds/new", function(req, res) {
-    res.render("new.ejs");
+    res.render("campgrounds/new");
 });
 
 //NEW
@@ -65,7 +66,42 @@ app.get("/campgrounds/:id", function(req, res) {
         if (err) {
             console.log("greska");
         } else {
-            res.render("show", { campground: camp });
+            res.render("campgrounds/show", { campground: camp });
+        }
+    });
+});
+
+// ===============
+// COMMENTS ROUTES
+// ===============
+
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+    // find campground by id
+    // send to template
+    Campground.findById(req.params.id, function(err, campground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", { campground: campground });
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+    Campground.findById(req.params.id, function(err, campground) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
         }
     });
 });
